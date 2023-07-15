d63572c979fc5d6746b0a9926a7c08a48a686f60
Xavier Tintin <xavier.tintin@epn.edu.ec>
2023-07-15 19:52:52 +0200

    Agregar proyecto

--- 01 - Android/app/src/main/java/com/example/movilescomp2023a/MainActivity.kt
+++ 01 - Android/app/src/main/java/com/example/movilescomp2023a/MainActivity.kt
@@ -1,9 +1,23 @@
 package com.example.movilescomp2023a
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    val callbackContenidoIntentExplicito = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            if (result.data != null) {
+                // Logica Negocio
+                val data = result.data
+                "${data?.getStringExtra(" nombreModificado ")}"
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
